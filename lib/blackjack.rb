7113d138b66de862a16b0559c9c8b27d3eaf621b
Chance Smith <chancesmith97@hotmail.com>
2019-01-30 22:10:43 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -82,7 +82,7 @@ def runner
   welcome
   card_total = initial_round
   until display_card_total(card_total) > 21
-    hit?(card_total)
+    card_total = hit?(card_total)
 
   end
   end_game(card_total)
